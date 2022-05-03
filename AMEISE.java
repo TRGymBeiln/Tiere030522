621800426f8094768c9133147f0b58fdaddd3bcf
paulaoe <paula.oexl@gmail.com>
2022-05-03 10:43:42 +0200

    Klasse AMEISE

---
+++ AMEISE.java
@@ -0,0 +1,16 @@
+public class AMEISE implements TIER
+{
+    private String name;
+    
+    public AMEISE()
+    {
+        name = "Ameise";
+
+    }
+
+    
+    public String machtGeraesch()
+    {
+        return "schabschabschab";
+    }
+}
