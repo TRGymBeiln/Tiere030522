f06848641053905d24c8cff0cb59919c91d44f9b
paulaoe <paula.oexl@gmail.com>
2022-05-03 10:30:10 +0200

    Klasse REGENWURM

---
+++ REGENWURM.java
@@ -0,0 +1,18 @@
+
+public class REGENWURM implements TIER
+{
+    private String name;
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    
+    public REGENWURM()
+    {
+        name = "Regenwurm";
+
+    }
+
+    
+    public String machtGeraesch()
+    {
+        return "schleimschleimschleim";
+    }
+}
