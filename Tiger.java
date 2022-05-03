5d44d1d74be6867aff22fb73367fac8b2fcdcd5d
WiederMueller <m.jonas.4@web.de>
2022-05-03 10:28:12 +0200

    Tiger

---
+++ Tiger.java
@@ -0,0 +1,23 @@
+public class Tiger implements TIER
+{
+    private String name;
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    /**
+     * Konstruktor für Objekte der Klasse ESEL
+     */
+    public Tiger()
+    {
+        name = "Tiger";
+
+    }
+
+    /**
+     * Gerausch eines Löwen
+     * 
+     * @return        gib das Gerausch zurueck
+     */
+    public String machtGeraesch()
+    {
+        return "ahhhhhhhh";
+    }
+}
